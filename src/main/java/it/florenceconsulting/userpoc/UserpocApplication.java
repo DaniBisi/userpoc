fc98ee40e5caaa2abfe0cd4c101f256e9e9cc52f
Daniele Bisignano <bisidevelop@gmail.com>
2022-11-13 11:50:13 +0100

    persistence config and docker settings

---
+++ src/main/java/it/florenceconsulting/userpoc/UserpocApplication.java
@@ -0,0 +1,13 @@
+package it.florenceconsulting.userpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/florenceconsulting/userpoc/ITUserpocApplication.java
@@ -0,0 +1,13 @@
+package it.florenceconsulting.userpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITUserpocApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
