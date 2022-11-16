6f4e99cf7973ec113040af0b129b406ec3c47ab0
Daniele Bisignano <bisidevelop@gmail.com>
2022-11-16 14:02:47 +0100

    update persistence generated value

--- src/main/java/it/florenceconsulting/userpoc/models/User.java
+++ src/main/java/it/florenceconsulting/userpoc/models/User.java
@@ -5,12 +5,17 @@
  */
 package it.florenceconsulting.userpoc.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 /**
  *
@@ -20,15 +25,32 @@ import lombok.Data;
 @Table(name = "user")
 @Data
 @Builder
-public class User{
-
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                @Parameter(name = "sequence_name", value = "user_sequence"),
+                @Parameter(name = "initial_value", value = "100"),
+                @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    @Column(name = "id")
     private Long id;
-    private String userName;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email")
     private String email;
-    private String lastName;
-    private String firstName;
-    private String cellPhone;
+    @Column(name = "lastname")
+    private String lastname;
+    @Column(name = "firstname")
+    private String firstname;
+    @Column(name = "cellphone")
+    private String cellphone;
 
 }
